518d8fcb77a7bdef366c06f73964f07c4127251b
Ray Hurst <rhurst1125@berkeley.edu>
2021-10-04 17:48:46 -0700

    got initial textbox working

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import numpy as np
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -10,6 +11,26 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/model")
+def model():
+    return render_template("model.html")
+
+
+#To use the predict button in our web-app
+@app.route("/predict", methods=["POST"])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)   
+    
+    return render_template("predict.html", prediction_text = 'Classifier result for end user is : {}'.format(output))
+
 
 
 if __name__ == "__main__":
